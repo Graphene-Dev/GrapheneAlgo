42040f302b48f8ac422f529f1c57ee95beb2ff1b
StealthHydra179 <aidenm888@gmail.com>
2021-03-18 10:01:25 -0400

    stuff

--- Algo/src/grapheneAlgo.java
+++ Algo/src/grapheneAlgo.java
@@ -1,11 +1,37 @@
+import javax.management.RuntimeErrorException;
 import java.util.*;
 
 
 public class grapheneAlgo {
+    private int length;
     private int difficulty;
+    private String toHash;
+    private String hash;
+    private boolean hashed = false;
 
-
-    public grapheneAlgo(int difficulty) {
+    //constructor
+    public grapheneAlgo(int length, int difficulty) {
+        this.length = length;
+    }
+    //accesors (no need to access the toHash)
+    public int getLength() {
+        return length;
+    }
+    public int getDifficulty() {
+        return difficulty;
+    }
+    public String getHash() throws Exception {
+        if (hashed) {
+            return hash;
+        } else {
+            throw new Exception("Not Hashed Yet");//Maybe just return -1?
+        }
 
     }
+
+
+
+    //add contents
+
+
 }
--- Algo/src/test.java
+++ Algo/src/test.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        grapheneAlgo test = new grapheneAlgo(1000);
+        grapheneAlgo test = new grapheneAlgo(1024, 5096);
 
     }
 }
